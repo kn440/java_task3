71179c9744dfdd2177a2cb826b019180d423dc5d
kn440 <78298987+kn440@users.noreply.github.com>
2023-03-10 14:32:15 +0300

    Add files via upload

---
+++ HW3_2_Stepanova.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+
+
+class HW3_2_Stepanova{
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.println("Первоначальный список "+ list1);
+
+        
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.printf("Список с удаленными четными числами %s", list1);
+       
+    }
+}
